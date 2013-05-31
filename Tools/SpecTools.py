23d7206a0fdd2aabdd381e2aa57dfdba4970bc34
cedricmontero <cedric.montero@esrf.fr>
2013-05-31 10:56:36 +0200

    Changes in SpecTools to implement get_ScanMeasurementsAlongTime function

--- Tools/SpecTools.py
+++ Tools/SpecTools.py
@@ -90,28 +90,40 @@ def get_ScanValueInSpecHeaderComment(sf,scannumber,field):
 	return value
 
 def get_ScanMeasurementsAlongTime(sf,scannumber,motorlabel):
-	"""
-	Retrieve the measurements of a motor label against time
-	@type sf : specfile object from specfile module
-	@type scannumber : integer
-	#type motorlabel : string
-	TODO : implementation not finished !!!
-	"""
-	# Store the scan data :
-	scan       = sf.select(str(scannumber))
-	data       = scan.data()
-	# Calculate time elements of the measurements :
-	eptime     = sf.epoch() + numpy.array(data[1,:])# Create a numpy 1D array with unix epoch time of each measurement
-	schedule   = numpy.array([datetime.datetime.fromtimestamp(values) for values in eptime])# Create a numpy 1D array of datetime instant of each measurement
-	eltime     = numpy.array(data[0,:])# Create a numpy 1D array of elapsed time [s] 
-	# Get the data measured mentionned by the motorlabel :
-	labelslist = scan.alllabels()
-	pos        = [idx for idx in range(len(labelslist)) if labelslist[idx] == motorlabel]
-	if pos == []:
-		print 'ERROR : The motorlabel required does not exist in the list of the labels of this scan.'
-	else:
-		print str(pos)
-	#return schedule,data
+    """
+    Retrieve the measurements of a motor label against time
+    @type sf : specfile object from specfile module
+    @type scannumber : integer
+    @type motorlabel : string
+    """
+    #TODO : rewrite this function based on the get_ScanMeasurement function
+    #TODO : extend this function to more than one motor to extract (induce list of string in input and variable number of output of list of output
+    def get_SingleScanMeasurementAlongTime(scannumber):
+        # Store the scan data:
+        scan       = sf.select(str(scannumber))
+        data       = scan.data()
+        # Find 'Epoch' label and motorlabel column position:
+        labelslist = scan.alllabels()
+        pos_epoch  = [idx for idx in range(len(labelslist)) if labelslist[idx] == 'Epoch'][0]
+        pos_motor  = [idx for idx in range(len(labelslist)) if labelslist[idx] == motorlabel][0]
+        # Calculate time elements of the measurements:
+        eptime     = sf.epoch() + numpy.array(data[pos_epoch,:])# Create a numpy 1D array with unix epoch time of each measurement
+        schedule   = numpy.array([datetime.datetime.fromtimestamp(values) for values in eptime])# Create a numpy 1D array of datetime instant of each measurement
+        if pos_motor == []:
+            print 'ERROR : The motorlabel required does not exist in the list of the labels of this scan.'
+        else:
+            measurement = data[pos_motor]
+        return schedule,measurement
+    if scannumber == '*':
+        schedule    = numpy.array([])
+        measurement = numpy.array([])
+        for scan in range(1,get_NumberOfScans(sf)+1):
+            scan_schedule,scan_measurement = get_SingleScanMeasurementAlongTime(scan)
+            schedule = numpy.hstack((schedule,scan_schedule))
+            measurement = numpy.hstack((measurement,scan_measurement))
+    else:
+        schedule,measurement = get_SingleScanMeasurementAlongTime(scannumber)
+    return schedule,measurement
 
 def get_ScanExposureTime(sf,scannumber):
 	"""
