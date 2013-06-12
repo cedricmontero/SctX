ba6ede4d590b83f84675f797c59c9820bdfdc5c1
cedricmontero <cedric.montero@esrf.fr>
2013-06-12 09:27:14 +0200

    Implement working directory definition plateform independently

--- _start.py
+++ _start.py
@@ -5,29 +5,34 @@
 ___author___   = 'Cédric Montero'
 ___contact___  = 'cedric.montero@esrf.fr'
 ___copyright__ = '2013, European Synchrotron Radiation Facility (ESRF)'
-___version___  = '0.0.1'
+___version___  = '0.1.beta'
 
 """ ______________________________SETTINGS_____________________________"""
+
+print('Welcome in SctX (v.%s) !'%___version___)
+
 
 """ External modules (preliminary installation could be require) """
 import sys
 sys.path.append('.'+'/Tools') # Add the Tools directory to the module path list
 import os
+import sys
 # ESRF library for I/O of 2D X-ray detector images
-try:
-    import fabio
-except ImportError:
-    print("No module to read ESRF edf images. Please install FabIO module.")
+#try:
+#    import fabio
+#except ImportError:
+#    print('No module to read ESRF edf images. Please install FabIO module.')
 
 """ Internal modules (local modules files) """
+import SettingsTools
 import SpecTools
 
 """ Set the working directory as experiment directory """
-# TODO : Check if just below there is PROCESS directory or not
-wdname = os.getcwd().rsplit('/',1)[0] + '/'
+print('Set the working directory :')
 
-os.chdir(wdname)# Change current working directory
-print 'Working directory is set to',os.getcwd()
+SDE = SettingsTools.SetDirectoryEnvironment()
+wdname = SDE.wdname
+print '    . Working directory is set to',os.getcwd()
 
 """ ______________________________ANALYSIS_____________________________"""
 
