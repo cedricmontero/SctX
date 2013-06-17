597671e22afa0d0f58ece1357cbf501148478dd8
cedricmontero <cedric.montero@esrf.fr>
2013-06-17 10:17:21 +0200

    Add Stretching tools for beamline device

---
+++ Models/Stretching.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+ Classes to handle stretching cell of ID13 within SPEC beamline layout
+"""
+___author___   = 'Cédric Montero'
+___contact___  = 'cedric.montero@esrf.fr'
+___copyright__ = '2013, European Synchrotron Radiation Facility (ESRF)'
+___version___  = '0.1.beta'
+
