5de1a6ad5dc6f550bea305184ebefe6b8c0eef95
cedricmontero <cedric.montero@esrf.fr>
2013-06-17 11:27:14 +0200

    Implement the class for streching utilities

--- Models/Stretching.py
+++ Models/Stretching.py
@@ -7,3 +7,38 @@ ___contact___  = 'cedric.montero@esrf.fr'
 ___copyright__ = '2013, European Synchrotron Radiation Facility (ESRF)'
 ___version___  = '0.1.beta'
 
+import sys
+sys.path.append('../Tools/')
+
+import numpy
+import SpecTools
+
+class Stretching:
+    def set_specfile(self,sfpath):
+        """
+        Set the specfile and retrieve the data
+        @param sfpath : specfile path
+        @type sfpath : string
+        """
+        self.specfile = SpecTools.specfile.Specfile(sfpath)
+        self.Stretch_Time,self.Stretch_Volt = SpecTools.get_ScanMeasurementsAlongTime(self.specfile,'*','strechVolt')
+        self.Stretch_Time,self.Stretch_N = SpecTools.get_ScanMeasurementsAlongTime(self.specfile,'*','strechN')
+        self.Stretch_Time,self.Stretch_MM = SpecTools.get_ScanMeasurementsAlongTime(self.specfile,'*','strechMM')
+        self.Stretch_Time_inHr = []
+        for i in self.Stretch_Time:
+            self.Stretch_Time_inHr.append((i-self.Stretch_Time[0]).days*24.+(i-self.Stretch_Time[0]).seconds/3600.)
+
+        def display_Stretch_Volt(self):
+            fig = pylab.figure(figsize=(7,4))
+            dpl = pylab.gcf()
+            dpl.canvas.set_window_title('Stretch_Volt')
+            ax = fig.add_subplot(111)
+            ax.plot(self.Stretch_Time_inHr,self.Stretch_Volt,'bx',markersize=2)
+            ax.set_xlabel('Elapsed Time [hr]')
+            ax.set_ylabel('Force [Volt]')
+            pylab.grid(True)
+
+
+
+if __name__ == '__main__':
+    S = Stretching()    
