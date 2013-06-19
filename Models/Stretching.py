d11b33db839670675ba49b108e610eeb4d21fe69
cedricmontero <cedric.montero@esrf.fr>
2013-06-19 08:46:10 +0200

    Add some displays and offset for displacement

--- Models/Stretching.py
+++ Models/Stretching.py
@@ -34,7 +34,7 @@ class Stretching:
             self.Stretch_Time_inHr.append((i-self.Stretch_Time[0]).days*24.+(i-self.Stretch_Time[0]).seconds/3600.)
         
     def display_Stretch_Volt(self):
-        fig = pylab.figure(figsize=(7,4))
+        fig = pylab.figure(figsize=(8,4))
         dpl = pylab.gcf()
         dpl.canvas.set_window_title('Stretch_Volt')
         ax = fig.add_subplot(111)
@@ -42,15 +42,37 @@ class Stretching:
         ax.set_xlabel('Elapsed Time [hr]')
         ax.set_ylabel('Force [Volt]')
         pylab.grid(True)
+    
+    def display_Stretch_N(self):
+        fig = pylab.figure(figsize=(8,4))
+        dpl = pylab.gcf()
+        dpl.canvas.set_window_title('Stretch_N')
+        ax = fig.add_subplot(111)
+        ax.plot(self.Stretch_Time_inHr,self.Stretch_N,'bx',markersize=2)
+        ax.set_xlabel('Elapsed Time [hr]')
+        ax.set_ylabel('Force [N]')
+        pylab.grid(True)
+    
+    def display_Stretch_MM(self):
+        fig = pylab.figure(figsize=(8,4))
+        dpl = pylab.gcf()
+        dpl.canvas.set_window_title('Stretch_MM')
+        ax = fig.add_subplot(111)
+        ax.plot(self.Stretch_Time_inHr,self.Stretch_MM,'bx',markersize=2)
+        ax.set_xlabel('Elapsed Time [hr]')
+        ax.set_ylabel('Disp [mm]')
+        pylab.grid(True)
 
-    def set_zero_force_offset(self,Yoffset_Volt = 0,Yoffset_N = 0):
+    def set_zero_force_offset(self,offset_Volt = 0,offset_N = 0):
         """
         Correct an offset value in the force measuremets (positive offset decrease the measurement value)
         """
-        self.Stretch_Volt = self.Stretch_Volt - Yoffset_Volt
-        self.Stretch_N    = self.Stretch_N - Yoffset_N
-        
+        self.Stretch_Volt = self.Stretch_Volt - offset_Volt
+        self.Stretch_N    = self.Stretch_N - offset_N
+    
+    def set_zero_disp_offset(self,offset_MM = 0):
+        self.Stretch_MM = self.Stretch_MM - offset_MM 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
     # Try to instantiate the class :
-    #S = Stretching()    
+    S = Stretching()    
