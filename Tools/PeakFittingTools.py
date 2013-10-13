bf2003cbdcf913ae5ca4de7ffef392fda5c1f39e
cedricmontero <cedric.montero@esrf.fr>
2013-10-13 12:37:46 +0200

    Add PeakFitting functionnalities

---
+++ Tools/PeakFittingTools.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+ This module offer fitting functionnalities to identify various peak positions in X-ray diffraction images
+"""
+___author___   = 'Cédric Montero'
+___contact___  = 'cedric.montero@esrf.fr'
+___copyright__ = 'European Synchrotron Radiation Facility (ESRF), Grenoble, France'
+___version___  = '0.1'
+
+""" External modules (preliminary installation could be require) """
+# Public modules :
+import numpy
+
+""" Internal modules (local modules files) """
+
+"""------------------------------------------------------------------------------------
+                    General functions on spec file informations 
+---------------------------------------------------------------------------------------"""
+          
+class PeakFitting
